7e6a581fb45f995cfb15e12aaf3a75937b65ce22
Jesse Carson <jmcarson222@gmail.com>
2023-06-21 10:23:38 -0500

    Setup Router

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import Home from "./HomePage";
+import About from "./AboutPage";
+import AuthWrapper from "./AuthWrapper";
+import Cart from "./CartPage";
+import Checkout from "./CheckoutPage";
+import Error from "./ErrorPage";
+import PrivateRoute from "./PrivateRoute";
+import ProductsPage from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+
+export {
+  Home,
+  About,
+  AuthWrapper,
+  Checkout,
+  Error,
+  PrivateRoute,
+  Cart,
+  ProductsPage as Products,
+  SingleProduct,
+};
