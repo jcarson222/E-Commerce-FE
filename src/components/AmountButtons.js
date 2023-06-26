175bb6b4099bbe92e501f9a12e2ca5b229fd9019
Jesse Carson <jmcarson222@gmail.com>
2023-06-26 12:18:39 -0500

    Cart amount/ add to cart

--- src/components/AmountButtons.js
+++ src/components/AmountButtons.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import styled from 'styled-components'
-import { FaPlus, FaMinus } from 'react-icons/fa'
+import React from "react";
+import styled from "styled-components";
+import { FaPlus, FaMinus } from "react-icons/fa";
 
-const AmountButtons = () => {
-  return <h4>amount buttons </h4>
-}
+const AmountButtons = ({ amount, increase, decrease }) => {
+  return (
+    <Wrapper className="amount-btns">
+      <button onClick={decrease} className="amount-btn" type="button">
+        <FaMinus />
+      </button>
+
+      <h2 className="amount">{amount}</h2>
+
+      <button onClick={increase} className="amount-btn" type="button">
+        <FaPlus />
+      </button>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.div`
   display: grid;
@@ -29,6 +41,6 @@ const Wrapper = styled.div`
   h2 {
     margin-bottom: 0;
   }
-`
+`;
 
-export default AmountButtons
+export default AmountButtons;
