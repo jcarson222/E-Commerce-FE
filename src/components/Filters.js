5d6586cebf6046b79181757a3356f4363e3ec69c
Jesse Carson <jmcarson222@gmail.com>
2023-06-27 14:39:48 -0500

    filter by color

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -85,6 +85,30 @@ const Filters = () => {
             </select>
           </div>
           {/* end companies */}
+          <div className="form-control">
+            <h5>colors</h5>
+            <div className="colors">
+              {colors.map((c, index) => {
+                return (
+                  <button
+                    key={index}
+                    name="color"
+                    style={{ background: c }}
+                    className={`${
+                      color === c ? "color-btn active" : "color-btn"
+                    }`}
+                    value={c}
+                    onClick={updateFilters}
+                  >
+                    {color === c ? <FaCheck /> : null}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+          {/* colors */}
+
+          {/* end colors */}
         </form>
       </div>
     </Wrapper>
--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -76,7 +76,7 @@ const filter_reducer = (state, action) => {
 
   if (action.type === UPDATE_FILTERS) {
     const { name, value } = action.payload;
-    // Dynamic object keys ([name]) allows you to update the value of said key (name) dynamically. In this case, whatever the value of 'value' is, we want also to be the value of 'name'. In other words, the value of the 'name' key (which is = "text") is set to the value of the 'value' key (whatever is in the search bar).
+    // Dynamic object keys ([name]) allows you to update the value of said key (name) dynamically. In this case, whatever the value of 'value' is, we want also to be the value of 'name'. In other words, the value of the 'name' key (which is = the element's name property value) is set to the value of the 'value' key (whatever is in the search bar or whatever filtering method).
     return { ...state, filters: { ...state.filters, [name]: value } };
   }
 
