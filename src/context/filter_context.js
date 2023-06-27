660c1a5125f8d3eee6191883c24c4fd30afab97e
Jesse Carson <jmcarson222@gmail.com>
2023-06-27 09:54:32 -0500

    filters - default values (dynamic max_price)

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -17,6 +17,16 @@ const initialState = {
   all_products: [],
   grid_view: true,
   sort: "price-lowest",
+  filters: {
+    text: "",
+    company: "all",
+    category: "all",
+    color: "all",
+    min_price: 0,
+    max_price: 0,
+    price: 0,
+    shipping: false,
+  },
 };
 
 const FilterContext = React.createContext();
@@ -25,24 +35,29 @@ export const FilterProvider = ({ children }) => {
   const { products } = useProductsContext();
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  // LOAD PRODUCTS
   // Since I can not pass 'products' from the useProductsContext straight into a filter_context state, I trigger the LOAD_PRODUCTS reducer func with useEffect when 'products' mounts, passing 'products' as the payload. In the filter_reducer func, I spread the payload into two different arrays (all_products and filtered_products).
   useEffect(() => {
     dispatch({ type: LOAD_PRODUCTS, payload: products });
   }, [products]);
 
+  // SORT PRODUCTS
   // Triggered based on 'product', and 'sort' state change.
   useEffect(() => {
     dispatch({ type: SORT_PRODUCTS });
   }, [products, state.sort]);
 
+  // GRID VIEW
   const setGridView = () => {
     dispatch({ type: SET_GRIDVIEW });
   };
 
+  // LIST VIEW
   const setListView = () => {
     dispatch({ type: SET_LISTVIEW });
   };
 
+  // UPDATE SORT
   const updateSort = (e) => {
     const value = e.target.value;
 
