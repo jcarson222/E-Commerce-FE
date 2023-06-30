e4a6ee18f507e50bfbcce525f68ab4361869d455
Jesse Carson <jmcarson222@gmail.com>
2023-06-30 09:12:12 -0500

    set up Auth0/ login-logout functions

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -10,6 +10,7 @@ const CartButtons = () => {
   // cart-btn-wrapper comes from the parent @media style
   const { closeSidebar } = useNavContext();
   const { total_items } = useCartContext();
+  const { loginWithRedirect, logout, myUser } = useUserContext();
   return (
     <Wrapper className="cart-btn-wrapper">
       <Link to="/cart" onClick={closeSidebar} className="cart-btn">
@@ -19,8 +20,15 @@ const CartButtons = () => {
           <span className="cart-value">{total_items}</span>
         </span>
       </Link>
-      <button type="button" className="auth-btn">
+      <button type="button" className="auth-btn" onClick={loginWithRedirect}>
         Login <FaUserPlus />
+      </button>
+      <button
+        type="button"
+        className="auth-btn"
+        onClick={() => logout({ returnTo: window.location.origin })}
+      >
+        Logout <FaUserMinus />
       </button>
     </Wrapper>
   );
--- src/index.js
+++ src/index.js
@@ -12,14 +12,29 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// //domain
+// dev - yypvst5623recfc5.us.auth0.com;
+// //clientId
+// xTJtzx9OHlsh9y2nOAf3zXfSqQrDK6Y8;
+
 root.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <NavProvider>
-          <App />
-        </NavProvider>
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain="dev-yypvst5623recfc5.us.auth0.com"
+    clientId="xTJtzx9OHlsh9y2nOAf3zXfSqQrDK6Y8"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <NavProvider>
+              <App />
+            </NavProvider>
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
 );
